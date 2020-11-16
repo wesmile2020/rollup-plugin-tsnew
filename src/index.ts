638c2f37645facaee1c317449ac36f22ec28fe70
weixiao <1211921908@qq.com>
2020-11-16 19:55:11 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import rollup from 'rollup';
+import fs from 'fs';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import Engine from './Engine';
+
+// const tslibPkg = require('tslib/package.json');
 
 interface Options {
     include?: FilterPattern;
@@ -14,15 +17,27 @@ const defaultOptions: Options = {
     check: true,
 };
 
+
 function typescript(options: Options): rollup.Plugin {
     const opts = { ...defaultOptions, ...options };
     const filter = createFilter(opts.include, opts.exclude);
     const cwd = process.cwd();
     
     const engine = new Engine(cwd);
+    // process tslib
+    // const tslibPath = require.resolve(`tslib/${tslibPkg.module}`);
+    // const tslib = fs.readFileSync(tslibPath, 'utf-8');
+    // const TSLIb_VIRTUAL = '\0tslib';
 
     return {
         name: 'rpt',
+
+        resolveId(source, importer) {
+            if (source === 'tslib') {
+
+            }
+            return null;
+        },
 
         transform(code: string, id: string) {
             if (!filter(id)) return;
